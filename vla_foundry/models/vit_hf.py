8ce77a565d5166df2f3de384e2e7d84e0c21864f
Tobias <tobihoeppe@gmail.com>
2026-05-09 23:26:43 +0200

    add possibility to overwrite resolution

--- vla_foundry/models/vit_hf.py
+++ vla_foundry/models/vit_hf.py
@@ -9,7 +9,12 @@ class ViTHF(BaseModel):
     def __init__(self, model_params: ViTHFParams, load_pretrained: bool = True):
         super().__init__(model_params)
         self.model_name = model_params.hf_pretrained
-        self.model = timm.create_model(self.model_name, num_classes=0, pretrained=load_pretrained)
+        kwargs = {"num_classes": 0, "pretrained": load_pretrained}
+        # Override the timm model's native input resolution if requested. timm
+        # interpolates pretrained position embeddings to the new size at load.
+        if getattr(model_params, "img_size", None) is not None:
+            kwargs["img_size"] = model_params.img_size
+        self.model = timm.create_model(self.model_name, **kwargs)
 
     def forward(self, image):
         image_embeddings = self.model.forward_intermediates(image)[0]
